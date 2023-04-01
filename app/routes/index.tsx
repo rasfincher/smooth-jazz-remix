f2dea8a2714c0d43cdef0a6bd2b20b5fa5f33d6d
Ras Fincher <ras@rasfincher.com>
2023-03-31 21:12:29 -0400

    Testing ssr

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,20 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/remix";
 import { getAuth } from "@clerk/remix/ssr.server";
 import { LoaderFunction, json } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
 import { WrapperFull } from "~/components/WrapperFull";
 
 export const loader: LoaderFunction = async (args) => {
   const auth = await getAuth(args);
   console.log("auth", auth.user);
-  return json({ user: auth.user }, { headers: { "cache-control": "no-store" } });
+  return json(auth.user);
 };
 
 export default function Index() {
   const user = useUser();
+  const data = useLoaderData<typeof loader>();
+  console.log("data", data);
   return (
     <WrapperFull>
       <div className="pb-10">
